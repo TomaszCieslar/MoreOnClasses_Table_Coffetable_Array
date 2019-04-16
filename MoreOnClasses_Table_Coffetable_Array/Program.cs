9add7a1a2e2b8be6570c2aec7384ca89f6c57fb6
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-04-16 09:20:30 +0200

    Add project files.

---
+++ MoreOnClasses_Table_Coffetable_Array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoreOnClasses_Table_Coffetable_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
